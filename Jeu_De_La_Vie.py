4461400e127f5b50d56942ae1559ac005a2306c1
Cuvelier-L <linec1634@gmail.com>
2024-04-03 09:40:32 +0200

    Update : réorganisation du programme

--- Jeu_De_La_Vie.py
+++ Jeu_De_La_Vie.py
@@ -12,25 +12,7 @@ nb_pixels_largeur = 8
 longueur_canva = nb_pixels_longueur * 50
 largeur_canva = nb_pixels_largeur * 50
 
-#fenetre 1 : explication des règles du jeu.
-fenetre1 = Tk()
-intro="Le jeu de la vie : des règles simples, une infinité de résolutions.\n\nLe jeu de la vie c’est 2 règles : Règle de survie, règle de naissance. \n\nAu début vous devrez choisir la taille du tableau puis la remplir comme vous le souhaitez, par la suite, vous verrez le développement des cellules*.\nA savoir que si une cellule* ne survie pas, elle meurt.\n*cellule = case pleine\n\n\nRègle de survie : \nSi une cellule est entourée de plus d’1 cellule et de moins de 4 cellules, elle survie au prochain tour.\n\nRègle de naissance :\nSi une case vide est entourée de exactement 3 cases, alors elle sera vivante le tour d’après."
-
-bouton=Button(fenetre1, text="Compris", command=fenetre1.quit)
-bouton.pack(side=BOTTOM, padx=150, pady=50)
-bouton.pack()
-
-introduction = Label(fenetre1, text=intro)
-
 """ ~~~ PARTIE FONCTIONNELLE ~~~ """
-
-
-
-""" ~~~ PARTIE EXECUTIVE ~~~ """
-
-introduction.pack()
-
-fenetre1.mainloop()
 
 class Tableau:
     def __init__(self, longueur, largeur):
@@ -55,3 +37,21 @@ class naissance:
         self.voisines = voisines
         self.case = case
     def lookforvide(self, case):
+        return 0
+
+
+
+""" ~~~ PARTIE EXECUTIVE ~~~ """
+
+#fenetre 1 : explication des règles du jeu.
+fenetre1 = Tk()
+intro="Le jeu de la vie : des règles simples, une infinité de résolutions.\n\nLe jeu de la vie c’est 2 règles : Règle de survie, règle de naissance. \n\nAu début vous devrez choisir la taille du tableau puis la remplir comme vous le souhaitez, par la suite, vous verrez le développement des cellules*.\nA savoir que si une cellule* ne survie pas, elle meurt.\n*cellule = case pleine\n\n\nRègle de survie : \nSi une cellule est entourée de plus d’1 cellule et de moins de 4 cellules, elle survie au prochain tour.\n\nRègle de naissance :\nSi une case vide est entourée de exactement 3 cases, alors elle sera vivante le tour d’après."
+
+bouton=Button(fenetre1, text="Compris", command=fenetre1.quit)
+bouton.pack(side=BOTTOM, padx=150, pady=50)
+bouton.pack()
+
+introduction = Label(fenetre1, text=intro)
+introduction.pack()
+
+fenetre1.mainloop()
