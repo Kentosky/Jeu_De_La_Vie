ff0e82c87fa525a8e58069f7f00e4d6996f71f95
Cuvelier-L <linec1634@gmail.com>
2024-04-03 09:31:59 +0200

    Update2 : fenêtre 1 terminée

--- Jeu_De_La_Vie.py
+++ Jeu_De_La_Vie.py
@@ -2,13 +2,26 @@
 # --- Le jeu de la vie re-créé par le groupe de TP avec Cuvelier Line, Villeret Baptiste et Besse Fabien  --- #
 from tkinter import *
 
-fenetre = Tk()
+#dimensions des pixels et du canva
+couleur = 'pink'
+nb_pixels_longueur = 10
+nb_pixels_largeur = 8
+longueur_canva = nb_pixels_longueur * 50
+largeur_canva = nb_pixels_largeur * 50
+
+#fenetre 1 : explication des règles du jeu.
+fenetre1 = Tk()
 intro="Le jeu de la vie : des règles simples, une infinité de résolutions.\n\nLe jeu de la vie c’est 2 règles : Règle de survie, règle de naissance. \n\nAu début vous devrez choisir la taille du tableau puis la remplir comme vous le souhaitez, par la suite, vous verrez le développement des cellules*.\nA savoir que si une cellule* ne survie pas, elle meurt.\n*cellule = case pleine\n\n\nRègle de survie : \nSi une cellule est entourée de plus d’1 cellule et de moins de 4 cellules, elle survie au prochain tour.\n\nRègle de naissance :\nSi une case vide est entourée de exactement 3 cases, alors elle sera vivante le tour d’après."
 
-introduction = Label(fenetre, text=intro)
+bouton=Button(fenetre1, text="Compris", command=fenetre1.quit)
+bouton.pack(side=BOTTOM, padx=150, pady=50)
+bouton.pack()
+
+introduction = Label(fenetre1, text=intro)
 introduction.pack()
 
-fenetre.mainloop()
+fenetre1.mainloop()
+
 class Tableau:
     def __init__(self, longueur, largeur):
         self.longueur = longueur
