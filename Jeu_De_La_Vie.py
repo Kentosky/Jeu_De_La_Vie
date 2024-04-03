f66ebdbac7aed58d56e0942bef3abe3937c008da
elBaptiste <villeret.baptiste@gmail.com>
2024-04-03 08:37:25 +0200

    classe Tableau (quadrillage composé de 0)

--- Jeu_De_La_Vie.py
+++ Jeu_De_La_Vie.py
@@ -1,2 +1,22 @@
-print("Hello world")
-print("test de commit")
+class Tableau:
+    def __init__(self, longueur, largeur):
+        self.longueur = longueur
+        self.largeur = largeur
+    def creation_tableau(longueur, largeur):
+        # creer le quadrillage
+        tableau_de_tableaux = []
+        for i in range(largeur):
+            ligne = []
+            for j in range(longueur):
+                ligne.append(0)
+            tableau_de_tableaux.append(ligne)
+
+        # Afficher le quadrillage
+        for ligne in tableau_de_tableaux:
+            print(ligne)
+        return tableau_de_tableaux
+
+
+
+
+
