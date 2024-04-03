da3616703c9e2322199222c61259c932f3d1f70e
Fabiboubinetitotet <fbztbag01@gmail.com>
2024-04-03 08:37:17 +0200

    introduction au jeu avec début du tkinter

--- Jeu_De_La_Vie.py
+++ Jeu_De_La_Vie.py
@@ -1,2 +1,11 @@
-print("Hello world")
-x = 2
+# -*- coding: utf-8 -*-
+# --- Le jeu de la vie re-créé par le groupe de TP avec Cuvelier Line, Villeret Baptiste et Besse Fabien  --- #
+from tkinter import *
+
+fenetre = Tk()
+
+introduction = Label(fenetre, text="Le jeu de la vie : des règles simples, une infinité de résolutions.\n\nLe jeu de la vie c’est 2 règles : Règle de survie, règle de naissance. \n\nAu début vous devrez choisir la taille du tableau puis la remplir comme vous le souhaitez, par la suite, vous verrez le développement des cellules*.\nA savoir que si une cellule* ne survie pas, elle meurt.\n*cellule = case pleine\n\n\nRègle de survie : \nSi une cellule est entourée de plus d’1 cellule et de moins de 4 cellules.\n\nRègle de naissance :\nSi une case vide est entourée de exactement 3 cases, alors elle sera vivante le tour d’après.")
+
+introduction.pack()
+
+fenetre.mainloop()
