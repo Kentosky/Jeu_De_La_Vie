3d339eeecb5d01459a15125426b7014857c28022
Fabiboubinetitotet <fbztbag01@gmail.com>
2024-05-07 12:01:02 +0200

    update de mon fichier perso

--- testDuJeuDeLaVie.py
+++ testDuJeuDeLaVie.py
@@ -1,61 +1,70 @@
-class vide:
-    def __init__(self, matrice, cox, coy):
-        self.cox = cox
-        self.coy = coy
-        self.matrice = matrice
-    def lookfornaissance(self, matrice, cox, coy):
+def cligno(matrice, cox, coy):
+    for i in range(3):
+        matrice[coy+i][cox] = 1
+    return matrice
 
-        left = 0
-        top = 0
-        cmpt = 0
-        bottom_OOR = 0
-        right_OOR = 0
+def hamecon(matrice, cox, coy):
+    for i in range(3):
+        matrice[coy+i][cox] = 1
+    matrice[coy][cox-1] = 1
+    matrice[coy+3][cox+1] = 1
+    matrice[coy+3][cox+2] = 1
+    matrice[coy+2][cox+2] = 1
+    return matrice
+def hamecon2(matrice, cox, coy):
+    for i in range(5):
+        matrice[coy-i-1][cox+i] = 1
+    for i in range(6):
+        matrice[coy-i][cox+i] = 1
+    matrice[coy-1][cox+1] = 0
+    matrice[coy-4][cox+4] = 0
+    matrice[coy-3][cox+2] = 0
+    matrice[coy][cox+1] = 1
+    matrice[coy-2][cox+3] = 1
+    matrice[coy-4][cox+5] = 1
+    return matrice
 
-        right = len(matrice[0])
-        bottom = len(matrice)
 
-        if cox == 0:
-            left = 1
+def canoe(matrice, cox, coy):
+    for i in range(4):
+        matrice[coy+i+1][cox+i] = 1
+    matrice[coy][cox] = 1
+    matrice[coy][cox+1] = 1
+    matrice[coy+4][cox+4] = 1
+    matrice[coy+3][cox+4] = 1
 
-        if coy == 0:
-            top = 1
+def penntadeca(matrice, cox, coy):
+    for i in range(10):
+        matrice[coy][cox + i] = 1
+    matrice[coy][cox + 2] = 0
+    matrice[coy][cox + 7] = 0
+    matrice[coy + 1][cox + 2] = 1
+    matrice[coy - 1][cox + 2] = 1
+    matrice[coy + 1][cox + 7] = 1
+    matrice[coy - 1][cox + 7] = 1
+    return matrice
 
-        if coy == bottom:
-            bottom_OOR += 1
-
-        if cox == right:
-            right_OOR += 1
-
-        if matrice[coy][cox] == 0:
-
-            for i in range(top-1, 2-bottom_OOR, 1):
-                for j in range(left-1, 2-right_OOR, 1):
-
-                    if matrice[coy + i][cox + j] == 1:
-                        cmpt += 1
-                        print("oui !")
-            if cmpt == 3:
-                matrice[coy][cox] = 1
-                return matrice
-
-            else:
-                return matrice
-
-        elif matrice[coy][cox] == 1:
-            return matrice
+def croix(matrice, cox, coy):
+    for i in range(4):
+        matrice[coy][cox+i+2] = 1
+        matrice[coy+7][cox+i+2] = 1
+        matrice[coy+2+i][cox] = 1
+        matrice[coy+i+2][cox+7] = 1
 
 tableau_de_tableaux = []
-for i in range(5):
+for i in range(20):
     ligne = []
-    for j in range(5):
+    for j in range(20):
         ligne.append(0)
     tableau_de_tableaux.append(ligne)
 
-for i in range(1, 4, 1):
-    tableau_de_tableaux[i][2] = 1
+len(tableau_de_tableaux)
+for i in range(len(tableau_de_tableaux)):
+    print(tableau_de_tableaux[i])
+
+print("\n\n")
+croix(tableau_de_tableaux,0,2)
 
 len(tableau_de_tableaux)
-print(tableau_de_tableaux)
-
-case = vide(tableau_de_tableaux, 3, 1)
-print(case.lookfornaissance(matrice=tableau_de_tableaux, cox=3, coy=2))
+for i in range(len(tableau_de_tableaux)):
+    print(tableau_de_tableaux[i])
