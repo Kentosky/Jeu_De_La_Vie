9315572b4e86741090fd1a875dc7a7ed2a1c7486
elBaptiste <villeret.baptiste@gmail.com>
2024-05-15 17:35:20 +0200

    Correction finale de la classe Cellule

--- Jeu_De_La_Vie.py
+++ Jeu_De_La_Vie.py
@@ -46,14 +46,12 @@ class Tableau:
         return tableau_de_tableaux
 
 class Cellule:
-    def __init__(self, matrice, coy, cox):      # dans cet ordre car on cree les colones avant les lignes
+    def __init__(self, matrice, coy, cox, temp_matrice):      # dans cet ordre car on cree les colones avant les lignes
         self.cox = cox                          # coordonnée x
         self.coy = coy                          # coordonnée y
         self.matrice = matrice                  # matrice
         # on créé une matrice temporaire pour pouvoir faire tous les déplacements sans risquer un effet domino
-        self.temp_matrice = []
-        for row in matrice:
-            self.temp_matrice.append(row[:])
+        self.temp_matrice = temp_matrice
 
     def regle(self):
         # on gère d'abord le cas où la cellule est collé a un coté du quadrillage
@@ -79,26 +77,25 @@ class Cellule:
                         cmptS += 1                                      # on incrémente le compteur de voisins
 
             if cmptS == 3 or cmptS == 4:                                # si la cellule a 2 ou 3 voisines (compteur=3 ou 4 car elle se compte elle-même)
-                return self.matrice                                     # rien ne se passe, la cellule reste en vie
+                self.temp_matrice[self.coy][self.cox] = 1               # rien ne se passe, la cellule reste en vie
+
             else:
                 self.temp_matrice[self.coy][self.cox] = 0               # sinon la cellule meurt, on modifie son état
-                return self.temp_matrice
+
         else:                                                           # 2eme cas:la cellule est morte
             cmptN = 0                                                   # compteur de voisins
             for i in range(top - 1, 2 - bottom_or, 1):                  # on regarde autour de la voisine
                 for j in range(left - 1, 2 - right_or, 1):              #               |
                     if self.matrice[self.coy + i][self.cox + j] == 1:   # si une voisine est vivante
                         cmptN += 1                                      # on incrémente le compteur
-            if cmptN == 4:                                              # si la cellule a 3 voisines (compteur=4 car elle se compte elle-même)
+            if cmptN == 3:                                              # si la cellule a 3 voisines (compteur=4 car elle se compte elle-même)
                 self.temp_matrice[self.coy][self.cox] = 1               # la cellule meurt, on modifie son état
-                return self.temp_matrice
-            else:
-                return self.matrice                                     # sinon rien ne se passe, la cellule reste morte
 
-    def appliquer_modifications(self): # Cette fonction applique les modifications de la matrice temporaire dans la matrice de base
-        for row in self.temp_matrice:
-            self.matrice.append(row[:])
-        return self.matrice
+            else:
+                self.temp_matrice[self.coy][self.cox] = 0               # sinon rien ne se passe, la cellule reste morte
+
+        return self.temp_matrice
+
 
 def cligno(matrice, cox, coy):
     for i in range(3):
